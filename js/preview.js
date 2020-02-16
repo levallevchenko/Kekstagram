fe4661ded606912694c6555ff31e02c3e5e69868
Анна Левченко <levallevchenko@gmail.com>
2020-02-16 17:24:26 +0300

    Изменение размера изображения

--- js/preview.js
+++ js/preview.js
@@ -2,6 +2,7 @@
 
 // preview.js
 (function () {
+  var ONE_HUNDRED_PERCENT = 100;
   var bigPicture = document.querySelector('.big-picture');
   var bigPictureClose = bigPicture.querySelector('.big-picture__cancel');
   var pictureImage = bigPicture.querySelector('.big-picture__img img');
@@ -13,6 +14,12 @@
   var socialComment = bigPicture.querySelector('.social__comment');
   var socialCommentCount = bigPicture.querySelector('.social__comment-count');
   var commentsLoader = bigPicture.querySelector('.comments-loader');
+
+  var imageUpload = document.querySelector('.img-upload');
+  var scaleControl = imageUpload.querySelector('.scale__control--value');
+  var scaleControlSmaller = imageUpload.querySelector('.scale__control--smaller');
+  var scaleControlBigger = imageUpload.querySelector('.scale__control--bigger');
+  var imageUploadPreviewImage = imageUpload.querySelector('.img-upload__preview img');
 
   // bigPicture.classList.remove('hidden');
 
@@ -58,5 +65,56 @@
 
   bigPictureClose.addEventListener('click', onBigPictureCloseClick);
   document.addEventListener('keycode', onBigPictureCloseEnterKeydown);
+
+  // Изменение размера
+
+  var setScale = function (value) {
+    var ScaleValue = value.substring(0, scaleControl.value.length - 1);
+    var scaleNumber = ScaleValue / ONE_HUNDRED_PERCENT;
+
+    imageUploadPreviewImage.style.transform = 'scale' + '(' + scaleNumber + ')';
+  };
+
+  var onScaleSmallerClick = function () {
+    var scaleValue = scaleControl.value.substring(0, scaleControl.value.length - 1);
+    if (scaleValue > 25) {
+      scaleControl.value = scaleValue - 25 + '%';
+    }
+    setScale(scaleControl.value);
+  };
+
+  var onScaleBiggerClick = function () {
+    var scaleValue = scaleControl.value.substring(0, scaleControl.value.length - 1);
+    var scaleValueNumber = parseInt(scaleValue, 10);
+    if (scaleValueNumber < 100) {
+      scaleControl.value = scaleValueNumber + 25 + '%';
+    }
+    setScale(scaleControl.value);
+  };
+
+  scaleControlSmaller.addEventListener('click', onScaleSmallerClick);
+  scaleControlBigger.addEventListener('click', onScaleBiggerClick);
+
 })();
 
+// Изменение размера
+
+// var onScaleSmallerClick = function () {
+//   var scaleValue = scaleControlValue.substring(0, scaleControlValue.length - 1);
+//   scaleControlValue = scaleValue - 25 + '%';
+//   return scaleControlValue;
+// };
+
+// var onScaleBiggerClick = function () {
+//   scaleControlValue += 25 + '%';
+// };
+
+// scaleControlSmaller.addEventListener('click', onScaleSmallerClick);
+// scaleControlBigger.addEventListener('click', onScaleBiggerClick);
+
+// var setScale = function (value) {
+//   var scale = value / ONE_HUNDRED_PERCENT;
+//   return scale;
+// };
+
+// setScale(scaleControlValue);
