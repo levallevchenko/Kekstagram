159cc75622231b0d1694be449fda2012bd2317f4
Анна Левченко <levallevchenko@gmail.com>
2020-02-29 21:54:31 +0300

    Добавляет обработку ошибок

--- js/request.js
+++ js/request.js
@@ -7,8 +7,18 @@
   var StatusCode = {
     OK: 200
   };
+  var picturesList = document.querySelector('.pictures');
+  var messageTemplate = document.querySelector('#error').content.querySelector('.error');
 
   var getRequest = function (method, url, onSuccess, onError, data) {
+
+    onError = function (message) {
+      var errorMessage = messageTemplate.cloneNode(true);
+      errorMessage.querySelector('.error__title').textContent = message;
+      errorMessage.querySelector('.error__button').style.display = 'none';
+      picturesList.appendChild(errorMessage);
+    };
+
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
@@ -17,14 +27,30 @@
       if (xhr.status === StatusCode.OK) {
         onSuccess(xhr.response);
       } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        var error;
+        switch (xhr.status) {
+          case 400:
+            error = 'Неверный запрос';
+            break;
+          case 401:
+            error = 'Пользователь не авторизован';
+            break;
+          case 404:
+            error = 'Ничего не найдено';
+            break;
+
+          default:
+            error = 'Cтатус ответа: : ' + xhr.status + ' ' + xhr.statusText;
+        }
+
+        if (error) {
+          onError(error);
+        }
       }
     });
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
+
     xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      onError('Запрос не успел выполниться за ' + xhr.timeout + ' мс');
     });
 
     xhr.timeout = TIMEOUT_IN_MS;
